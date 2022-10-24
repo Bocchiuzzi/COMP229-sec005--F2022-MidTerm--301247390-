c0f1ab567378d1b22a77556a4c76a0201e0ec074
Bocchiuzzi <bocchiuzzi@hotmail.com>
2022-10-24 12:58:56 -0400

    Root/modules/db
    Fixed Root folder issue
    did npm install
    connected to my new MongoDB

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 // Do not expose your credentials in your code.
-let atlasDB = "mongodb+srv://<username>:<passoword>@<cluster>/todo?retryWrites=true&w=majority";
+let atlasDB = "mongodb+srv://Brandon:midterm@cluster005.angkzp4.mongodb.net/todo?retryWrites=true&w=majority";
 
 // Database setup
 let mongoose = require('mongoose');
